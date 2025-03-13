9e630ec140f58243d8999b2bfa8e99c9cf33c771
Muhammad Faizan <muhammadfaizanakx@gmail.com>
2025-03-13 23:27:36 +0500

    basic project setup + tailwindcss installation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-black font-bold text-9xl hover:skew-12 transition duration-500 ">app</div>
+    </>
+  );
+}
+
+export default App;
